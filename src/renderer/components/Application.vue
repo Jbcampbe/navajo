044a7591df5641dac7d6fe4175a7fbb130b4148c
Josiah Campbell <jbcampbe@iupui.edu>
2018-02-28 20:27:51 -0500

    adding editorconfig, removing default landing page, starting alphabetjs

---
+++ src/renderer/components/Application.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app-container">
+    Sup World
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'application'
+  }
+</script>
+
+<style>
+</style>
---
+++ src/renderer/utils/alphabet.js
@@ -0,0 +1,9 @@
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+const alphabetMap = {}
+alphabet.forEach(letter => { alphabetMap[letter] = ' ' })
+
+export default {
+  alphabet,
+  alphabetMap
+}
