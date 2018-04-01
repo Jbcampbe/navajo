53559816082aa2a26bb2acfd10e771329d43672d
Josiah Campbell <jbcampbe@iupui.edu>
2018-04-01 17:16:03 -0400

    finished functionality of the chart. still needs much styling

--- src/renderer/components/Application.vue
+++ src/renderer/components/Application.vue
@@ -6,23 +6,23 @@
     </div>
     <nav class="navigation-bar">
       <div class="link-container">
-        <router-link to="/shift">
+        <router-link class="nav-link" to="/shift">
           <i class="material-icons navigation-icon">keyboard_tab</i>
           <span class="link-text">CAESAR</span>
         </router-link>
-        <router-link to="/vigenere">
+        <router-link class="nav-link" to="/vigenere">
           <i class="material-icons navigation-icon">grid_on</i>
           <span class="link-text">VIGENERE</span>
         </router-link>
-        <router-link to="/anagram">
+        <router-link class="nav-link" to="/anagram">
           <i class="material-icons navigation-icon">swap_horiz</i>
           <span class="link-text">ANAGRAM</span>
         </router-link>
-        <router-link to="/columnar">
+        <router-link class="nav-link" to="/columnar">
           <i class="material-icons navigation-icon">view_column</i>
           <span class="link-text">COLUMNAR</span>
         </router-link>
-        <router-link to="/substitution">
+        <router-link class="nav-link" to="/substitution">
           <i class="material-icons navigation-icon">space_bar</i>
           <span class="link-text">SUBSTITUTION</span>
         </router-link>
@@ -33,7 +33,14 @@
             <span>FREQUENCY</span>
         </button>
         <div class="drawer">
-            <span>Drawer</span>
+            <div class="chart-container">
+              <highcharts :options="options"></highcharts>
+            </div>
+            <div>
+              <button @click="showSingleLetterFrequencies">1</button>
+              <button @click="showDigramFrequencies">2</button>
+              <button @click="showTrigramFrequencies">3</button>
+            </div>
         </div>
     </div>
   </div>
@@ -41,6 +48,7 @@
 
 <script>
   import CipherInputs from '@/components/cipher-inputs'
+  import { getSingleFrequencies, getDigramFrequencies, getTrigramFrequencies } from '@/utils/frequency'
 
   export default {
     name: 'application',
@@ -48,21 +56,104 @@
       CipherInputs
     },
 
+    computed: {
+      getFrequencies () {
+        if (this.frequencyMode === 1) {
+          return getSingleFrequencies
+        } else if (this.frequencyMode === 2) {
+          return getDigramFrequencies
+        }
+
+        return getTrigramFrequencies
+      },
+
+      options () {
+        let frequencies = this.getFrequencies(this.$store.state.Cipher.ciphertext)
+
+        return {
+          title: {
+            text: ''
+          },
+
+          xAxis: {
+            categories: Object.keys(frequencies),
+            lineColor: '#FFFFFF',
+            lineWidth: 3,
+            tickAmount: 0,
+            gridLineColor: 'transparent',
+            labels: {
+              style: {
+                color: '#FFFFFF',
+                fontSize: '14px'
+              }
+            }
+          },
+
+          yAxis: {
+            title: {
+              text: ''
+            },
+            lineColor: '#FFFFFF',
+            lineWidth: 3,
+            tickAmount: 0,
+            gridLineColor: 'transparent',
+            labels: {
+              style: {
+                color: 'transparent'
+              }
+            }
+          },
+
+          series: [{
+            type: 'column',
+            data: Object.values(frequencies),
+            showInLegend: false,
+            dataLabels: {
+              color: '#FFFFFF'
+            }
+          }],
+
+          chart: {
+            backgroundColor: 'transparent',
+            showAxes: true,
+            height: 700,
+            width: 1000
+          },
+          colors: ['#FFFFFF']
+        }
+      }
+    },
+
     data () {
       return {
-        drawerClosed: true
+        drawerClosed: false,
+        frequencyMode: 1
       }
     },
 
     methods: {
       toggleDrawer () {
         this.drawerClosed = !this.drawerClosed
+      },
+
+      showSingleLetterFrequencies () {
+        this.frequencyMode = 1
+      },
+
+      showDigramFrequencies () {
+        this.frequencyMode = 2
+      },
+
+      showTrigramFrequencies () {
+        this.frequencyMode = 3
       }
     }
   }
 </script>
 
 <style lang="scss">
+  $drawer-size: calc(100vw * .8);
+
   body {
     font-family: RobotoDraft, Roboto, sans-serif;
     margin: 0;
@@ -105,7 +196,7 @@
       justify-content: space-around;
     }
 
-    a {
+    .nav-link {
       width: 100px;
       height: 85px;
       justify-content: center;
@@ -142,25 +233,34 @@
   }
 
   .drawer-closed {
-      transform: translate(900px);
+      transform: translate($drawer-size);
       transition: all .5s ease-out;
   }
 
   .drawer-handle {
     height: 500px;
     width: 30px;
-    background-color: red;
+    background-color: #E3341B;
     border: none;
 
     span {
       text-orientation: sideways;
       writing-mode: vertical-lr;
+      transform: rotate(180deg);
+      color: white;
+      font-size: 14px;
     }
   }
 
   .drawer {
     height: 80%;
-    width: 900px;
-    background-color: red;
+    width: $drawer-size;
+    background-color: #E3341B;
+    display: flex;
+    align-items: center;
+  }
+
+  .highcharts-credits {
+    display: none;
   }
 </style>
