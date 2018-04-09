7ef4a16de4d0209e910c7be03203fa3a4173527a
Josiah Campbell <jbcampbe@iupui.edu>
2018-04-09 11:52:24 -0400

    creating polyalphabetic util and tests

---
+++ src/renderer/utils/polyalphabetic.js
@@ -0,0 +1,14 @@
+import { alphabet } from './alphabet'
+
+/**
+ * decrypts vigenere ciphers
+ *
+ * @param  {string} message - encrypted message. should be all lowercase with no spaces
+ * @param  {string} keyword - keyword for the decrypting
+ * @returns {string} - plaintext message
+ */
+function decipherVigenere(message, keyword) {
+  for (let i = 0; i < message.length; i++) {
+
+  }
+}
---
+++ test/unit/specs/utils/polyalphabetic.spec.js
@@ -0,0 +1,11 @@
+import { decipherVigenere } from '@/utils/polyalphabetic'
+
+const vigenereMessage = 'sithisuetosnatreehmgnaarrpciehkwisrooginrpzrpeyl'
+const vigenerePlaintext = 'thisistoensuretheanagramcipherisworkingproperlyz'
+const vigenereKeyword = 'thekeyissherlock'
+
+describe('polyalphabetic.js', () => {
+  it('should decrypt vigenere ciphers', () => {
+
+  })
+})
