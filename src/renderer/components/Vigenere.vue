2054c7bcad5fd66b482aeb3d05c332d6708be6bc
Amos Endersen <amenders@iupui.edu>
2018-04-20 15:21:04 -0400

    edited vigenere toolkit ui

--- src/renderer/components/Vigenere.vue
+++ src/renderer/components/Vigenere.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="view">
     <div class="crypto-tool-list">
-      <button class="action-btn" @click="showKasiski = !showKasiski">KASISKI</button>
+      <button class="action-btn" v-bind:class = '{open: showKasiski}' @click="showKasiski = !showKasiski">KASISKI</button>
       <div class="vigenere-tool kasiki" :class="{ hidden: !showKasiski }">
         <span>Possible Key Factors:</span>
         <div class="factor-container">
@@ -11,13 +11,13 @@
         </div>
       </div>
 
-      <button class="action-btn" @click="showIC = !showIC">IC</button>
+      <button class="action-btn" v-bind:class = '{open: showIC}' @click="showIC = !showIC">IC</button>
       <div class="vigenere-tool kasiki" :class="{ hidden: !showIC }">
         <span>Possible Key Factors:</span>
         <span>2, 3, 5</span>
       </div>
 
-      <button class="action-btn" @click="showLFA = !showLFA">LOW FREQUENCY</button>
+      <button class="action-btn" v-bind:class = '{open: showLFA}' @click="showLFA = !showLFA">LOW FREQUENCY</button>
       <div class="vigenere-tool kasiki" :class="{ hidden: !showLFA }">
         <span>Possible Key Factors:</span>
         <span>2, 3, 5</span>
@@ -50,7 +50,11 @@
 
     computed: {
       kasiskiFactors () {
-        return kasiski(this.$store.state.Cipher.ciphertext)
+        if (kasiski(this.$store.state.Cipher.ciphertext).length > 0) {
+          return kasiski(this.$store.state.Cipher.ciphertext)
+        } else {
+          return '-'
+        }
       }
     }
   }
@@ -60,9 +64,10 @@
   .vigenere-tool {
     width: 100%;
     max-width: 250px;
-    height: 85px;
+    height: 70px;
     background-color: #FFFFFF;
-    border-radius: 4px;
+    border-bottom-left-radius: 4px;
+    border-bottom-right-radius: 4px;
     opacity: .8709;
     color: #E3341B;
     display: flex;
@@ -77,5 +82,10 @@
 
   .hidden {
     display: none;
+  }
+
+  .open {
+    border-bottom-left-radius: 0px;
+    border-bottom-right-radius: 0px;
   }
 </style>
